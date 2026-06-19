4c66c3286182efcc44a6e357765545c555d476ca
zowork <sdkwork@zowalk.com>
2026-06-19 11:11:35 +0800

    chore: sync workspace updates

---
+++ crates/sdkwork-router-cms-open-api/src/http_route_manifest.rs
@@ -0,0 +1,40 @@
+use sdkwork_web_core::{HttpMethod, HttpRoute, HttpRouteManifest};
+
+use crate::paths;
+
+const HTTP_ROUTES: &[HttpRoute] = &[
+    HttpRoute::api_key(
+        HttpMethod::Get,
+        paths::ENTRIES,
+        "cms",
+        "cms.entries.list",
+    ),
+    HttpRoute::api_key(
+        HttpMethod::Get,
+        paths::ENTRY_BY_ID,
+        "cms",
+        "cms.entries.retrieve",
+    ),
+    HttpRoute::api_key(
+        HttpMethod::Get,
+        paths::ENTRIES_RESOLVE,
+        "cms",
+        "cms.entries.resolve",
+    ),
+    HttpRoute::api_key(
+        HttpMethod::Get,
+        paths::PAGES_RESOLVE,
+        "cms",
+        "cms.pages.resolve",
+    ),
+    HttpRoute::api_key(
+        HttpMethod::Get,
+        paths::FEED_ITEMS,
+        "cms",
+        "cms.feeds.items.list",
+    ),
+];
+
+pub fn open_route_manifest() -> HttpRouteManifest {
+    HttpRouteManifest::new(HTTP_ROUTES)
+}
